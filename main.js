a65e026c49ffc176634634ca5f543b6eb2d9a3cb
Mark <hello@markhaehnel.de>
2016-03-23 12:23:42 +0100

    Switched to TMI.js CDN, fixed chat config

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-var channels = ['rocketbeanstv'], // Channels to initially join
-	fadeDelay = false, // Set to false to disable chat fade
+var fadeDelay = false, // Set to false to disable chat fade
 	showChannel = false, // Show repespective channels if the channels is longer than 1
 	useColor = true, // Use chatters' colors or to inherit
 	showBadges = true, // Show chatters' badges
@@ -9,10 +8,6 @@ var channels = ['rocketbeanstv'], // Channels to initially join
 	showHosting = false, // Show when the channel is hosting or not
 	showConnectionNotices = true; // Show messages like "Connected" and "Disconnected"
 
-
-
-
-
 var chat = document.getElementById('chat'),
 	defaultColors = ['rgb(255, 0, 0)','rgb(0, 0, 255)','rgb(0, 128, 0)','rgb(178, 34, 34)','rgb(255, 127, 80)','rgb(154, 205, 50)','rgb(255, 69, 0)','rgb(46, 139, 87)','rgb(218, 165, 32)','rgb(210, 105, 30)','rgb(95, 158, 160)','rgb(30, 144, 255)','rgb(255, 105, 180)','rgb(138, 43, 226)','rgb(0, 255, 127)'],
 	randomColorsChosen = {},
@@ -21,7 +16,11 @@ var chat = document.getElementById('chat'),
 					debug: false,
 					reconnect: true
 				},
-			channels: channels
+			connection: {
+				cluster: "aws",
+				secure: true
+			},
+			channels: ['rocketbeanstv']
 		},
 	client = new irc.client(clientOptions);
 
